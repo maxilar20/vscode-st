19b235e8896b5c56ac3f50751eeabc134f9f21cb
maxilar20 <dorne99@hotmail.com>
2024-04-10 10:39:29 +0100

    Added tabulation

--- src/formatter.ts
+++ src/formatter.ts
@@ -1,5 +1,5 @@
-'use strict';
-import * as vscode from 'vscode';
+"use strict";
+import * as vscode from "vscode";
 
 export class STFormatterProvider implements vscode.DocumentFormattingEditProvider {
     private functions: Array<string> = [];
@@ -15,49 +15,189 @@ export class STFormatterProvider implements vscode.DocumentFormattingEditProvide
 
         let out = [];
         this.functions = [
-            'abs', 'sin', 'mod', 'abs', 'acos', 'asin', 'atan', 'cos', 'exp',
-            'expt', 'ln', 'log', 'sin', 'sqrt', 'tan', 'sel', 'mux', 'shl',
-            'shr', 'rol', 'ror', 'add', 'div', 'mul', 'sub', 'limit', 'max',
-            'min', 'adr', 'adrinst', 'size', 'sizeof', 'bit_adr', 'bit_trunc',
-            'rs', 'sr', 'ton', 'tp', 'tof', 'trunc', 'ctd', 'ctu', 'ctud', 'r_trig',
-            'f_trig', 'move', 'concat', 'delete', 'find', 'insert', 'left', 'len',
-            'replace', 'right', 'rtc', 'mid', 'sema', 'round', 'floor', 'ceil',
-            'unpack', 'ref', '__new', '__delete',
+            "abs",
+            "sin",
+            "mod",
+            "abs",
+            "acos",
+            "asin",
+            "atan",
+            "cos",
+            "exp",
+            "expt",
+            "ln",
+            "log",
+            "sin",
+            "sqrt",
+            "tan",
+            "sel",
+            "mux",
+            "shl",
+            "shr",
+            "rol",
+            "ror",
+            "add",
+            "div",
+            "mul",
+            "sub",
+            "limit",
+            "max",
+            "min",
+            "adr",
+            "adrinst",
+            "size",
+            "sizeof",
+            "bit_adr",
+            "bit_trunc",
+            "rs",
+            "sr",
+            "ton",
+            "tp",
+            "tof",
+            "trunc",
+            "ctd",
+            "ctu",
+            "ctud",
+            "r_trig",
+            "f_trig",
+            "move",
+            "concat",
+            "delete",
+            "find",
+            "insert",
+            "left",
+            "len",
+            "replace",
+            "right",
+            "rtc",
+            "mid",
+            "sema",
+            "round",
+            "floor",
+            "ceil",
+            "unpack",
+            "ref",
+            "__new",
+            "__delete",
 
-            '[A-Za-z_]*(_TO_)[A-Za-z_]*', '(?:TO_|FROM_|TRUNC_)[A-Za-z_]*'
+            "[A-Za-z_]*(_TO_)[A-Za-z_]*",
+            "(?:TO_|FROM_|TRUNC_)[A-Za-z_]*",
         ];
 
         this.keywords = [
-            'true', 'false', 'exit', 'continue', 'return', 'constant', 'retain',
-            'public', 'private', 'protected', 'abstract', 'persistent', 'internal',
-            'final', 'of', 'else', 'elsif', 'then', '__try', '__catch', '__finally',
-            '__endtry', 'do', 'to', 'by', 'task', 'with', 'using', 'uses', 'from',
-            'until', 'or', 'or_else', 'and', 'and_then', 'not', 'xor', 'nor', 'ge',
-            'le', 'eq', 'ne', 'gt', 'lt', 'extends', 'implements', 'this', 'super',
-            '(?:T|DT|TOD|D)#(?=[0-9])',
+            "true",
+            "false",
+            "exit",
+            "continue",
+            "return",
+            "constant",
+            "retain",
+            "public",
+            "private",
+            "protected",
+            "abstract",
+            "persistent",
+            "internal",
+            "final",
+            "of",
+            "else",
+            "elsif",
+            "then",
+            "__try",
+            "__catch",
+            "__finally",
+            "__endtry",
+            "do",
+            "to",
+            "by",
+            "task",
+            "with",
+            "using",
+            "uses",
+            "from",
+            "until",
+            "or",
+            "or_else",
+            "and",
+            "and_then",
+            "not",
+            "xor",
+            "nor",
+            "ge",
+            "le",
+            "eq",
+            "ne",
+            "gt",
+            "lt",
+            "extends",
+            "implements",
+            "this",
+            "super",
+            "(?:T|DT|TOD|D)#(?=[0-9])",
             // '(?:T|DT|TOD|D)#[0-9\\:\\-\\_yYmMdDhHsS]+',
-            'var_(?:input|output|in_out|temp|global|access|external)'
+            "var_(?:input|output|in_out|temp|global|access|external)",
         ];
 
         this.types = [
-            'AT', 'BOOL', 'BYTE', '(?:D|L)?WORD', 'U?(?:S|D|L)?INT', 'L?REAL',
-            'TIME(?:_OF_DAY)?', 'TOD', 'DT', 'DATE(?:_AND_TIME)?', 'W?STRING',
-            'ARRAY', 'ANY', 'ANY_(?:NUM|INT|REAL)'];
+            "AT",
+            "BOOL",
+            "BYTE",
+            "(?:D|L)?WORD",
+            "U?(?:S|D|L)?INT",
+            "L?REAL",
+            "TIME(?:_OF_DAY)?",
+            "TOD",
+            "TON",
+            "DT",
+            "DATE(?:_AND_TIME)?",
+            "W?STRING",
+            "ARRAY",
+            "ANY",
+            "ANY_(?:NUM|INT|REAL)",
+        ];
 
         this.ends = [
-            'var', 'program', 'if', 'case', 'while', 'for', 'repeat', 'function',
-            'function_block', 'struct', 'configuration', 'tcp', 'resource',
-            'channel', 'library', 'folder', 'binaries', 'includes', 'sources',
-            'action', 'step', 'initial_step', 'transition', 'type', 'namespace',
-            'implementation', 'interface', 'property', 'get', 'set', 'method', 'union', 'class'
+            "var",
+            "program",
+            "if",
+            "case",
+            "while",
+            "for",
+            "repeat",
+            "function",
+            "function_block",
+            "struct",
+            "configuration",
+            "tcp",
+            "resource",
+            "channel",
+            "library",
+            "folder",
+            "binaries",
+            "includes",
+            "sources",
+            "action",
+            "step",
+            "initial_step",
+            "transition",
+            "type",
+            "namespace",
+            "implementation",
+            "interface",
+            "property",
+            "get",
+            "set",
+            "method",
+            "union",
+            "class",
         ];
 
         // Do not format this strings
         this.skipString = [
             `["']{1}[^\"\'\\\\]*(?:\\\\[\\s\\S][^"'\\\\]*)*["']{1}`, // All strings in quotes
-            '\\(\\*[\\s\\S]*?\\*\\)', // All comments in braces
-            '\\/\\*[\\s\\S]*?\\*\\/', // All comments in slashes
-            '\\/\\/[^\\n]*\\n' // All single line comments
+            "\\(\\*[\\s\\S]*?\\*\\)", // All comments in braces
+            "\\/\\*[\\s\\S]*?\\*\\/", // All comments in slashes
+            "\\/\\/[^\\n]*\\n", // All single line comments
         ];
 
         let text = document.getText();
@@ -66,9 +206,12 @@ export class STFormatterProvider implements vscode.DocumentFormattingEditProvide
         text = this.capitalize(text);
 
         out.push(
-            new vscode.TextEdit(new vscode.Range(
-                new vscode.Position(0, 0),
-                document.lineAt(document.lineCount - 1).range.end), text
+            new vscode.TextEdit(
+                new vscode.Range(
+                    new vscode.Position(0, 0),
+                    document.lineAt(document.lineCount - 1).range.end
+                ),
+                text
             )
         );
 
@@ -77,69 +220,131 @@ export class STFormatterProvider implements vscode.DocumentFormattingEditProvide
 
     spaces(text: string): string {
         // Delete all double spaces
-        while(text.match(/(?<!^| )  /gm)!== null) {
-            text = text.replace(/(?<!^| )  /gm, " ");
+        const regex = /(?<!^| )  /gm;
+        const regex2 = /\t/g;
+
+        while (text.match(regex) !== null) {
+            text = text.replace(regex, " ");
+        }
+
+        while (text.match(regex2) !== null) {
+            text = text.replace(regex2, " ");
+        }
+
+        while (text.match(regex) !== null) {
+            text = text.replace(regex, " ");
         }
 
         // Delete space between func name and (
         // ABS ( to ABS(
-        let regEx = new RegExp(`\\b(?:${this.functions.join('|')})\\b\\s+\\(`, "ig");
+        let regEx = new RegExp(`\\b(?:${this.functions.join("|")})\\b\\s+\\(`, "ig");
         text = text.replace(regEx, (match) => {
-            return match.replace(/\s+/, '');
+            return match.replace(/\s+/, "");
         });
 
         // Add space after keywords
         // IF( to IF (
         regEx = new RegExp(`\\b(IF|WHILE|CASE)\\(`, "ig");
         text = text.replace(regEx, (match, key) => {
-            return key !== undefined ? key + ' (' : match;
+            return key !== undefined ? key + " (" : match;
         });
 
         // Add before after keywords
         // )THEN to ) THEN
         regEx = new RegExp(`\\)(THEN|DO|OF)\\b`, "ig");
         text = text.replace(regEx, (match, key) => {
-            return key !== undefined ? ') ' + key : match;
+            return key !== undefined ? ") " + key : match;
         });
 
         let addSpace = {
-            csb: ['\\*\\)', '\\*\\/', '(?<=.)\\/\\/', '(?<=.)\\(\\*', '(?<=.)\\/\\*'],
-            csa: ['\\(\\*', '\\/\\*', '\\/\\/'],
-            ss:  [':=', '<>', '>=', '<=', '=>', '\\+', '\\-', '\\/'],
-            sb:  ['(?<!<|>|:)=', ':', '(?<!\\*)\\*(?!\\*)', '<', '(?<!=|<)>'],
-            sa:  ['=(?!>| )', ':(?!=)', '\\*(?!\\*|;)', ',', '<(?!=|>)', '>(?!=)']
+            csb: ["\\*\\)", "\\*\\/", "(?<=.)\\/\\/", "(?<=.)\\(\\*", "(?<=.)\\/\\*"],
+            csa: ["\\(\\*", "\\/\\*", "\\/\\/"],
+            ss: [":=", "<>", ">=", "<=", "=>", "\\+", "\\-", "\\/"],
+            sb: ["(?<!<|>|:)=", ":", "(?<!\\*)\\*(?!\\*)", "<", "(?<!=|<)>"],
+            sa: ["=(?!>| )", ":(?!=)", "\\*(?!\\*|;)", ",", "<(?!=|>)", ">(?!=)"],
         };
 
-        regEx = new RegExp(`(?<! |\\t)(${addSpace.csb.join('|')})`, "ig");
+        regEx = new RegExp(`(?<! |\\t)(${addSpace.csb.join("|")})`, "ig");
         text = text.replace(regEx, (match, sign) => " " + sign);
-        regEx = new RegExp(`(${addSpace.csa.join('|')})(?! |\\t)`, "ig");
+        regEx = new RegExp(`(${addSpace.csa.join("|")})(?! |\\t)`, "ig");
         text = text.replace(regEx, (match, sign) => sign + " ");
 
-        regEx = new RegExp(`${this.skipString.join('|')}|(?<! |\\t)(${addSpace.ss.join('|')}|${addSpace.sb.join('|')})`, "ig");
-        text = text.replace(regEx, (match, sign) => sign !== undefined ? " " + sign : match);
-        regEx = new RegExp(`${this.skipString.join('|')}|(${addSpace.ss.join('|')}|${addSpace.sa.join('|')})(?! |\\t)`, "ig");
-        text = text.replace(regEx, (match, sign) => sign !== undefined ? sign + " " : match);
+        regEx = new RegExp(
+            `${this.skipString.join("|")}|(?<! |\\t)(${addSpace.ss.join("|")}|${addSpace.sb.join("|")})`,
+            "ig"
+        );
+        text = text.replace(regEx, (match, sign) => (sign !== undefined ? " " + sign : match));
+        regEx = new RegExp(
+            `${this.skipString.join("|")}|(${addSpace.ss.join("|")}|${addSpace.sa.join("|")})(?! |\\t)`,
+            "ig"
+        );
+        text = text.replace(regEx, (match, sign) => (sign !== undefined ? sign + " " : match));
 
         // Delete all spaces at the end of the lines
-        text = text.split("\n").map(el => el.trimEnd()).join("\n");
+        text = text
+            .split("\n")
+            .map((el) => el.trimEnd())
+            .join("\n");
 
         // regEx = new RegExp(`[\s]+:(?!=)`, "ig");
         text = text.replace(/[\s]+:(?!=)/g, ":");
         // regEx = new RegExp(`;[\s]+\n$`, "ig");
         text = text.replace(/;[\s]+\n$/g, ";\n");
 
+        // NOTES:
+        // - Comments at the end of IF does not work
+        // - Comments need more work
+        var lines = text.split(/\r\n|\r|\n/);
+        var tab = 0;
 
+        let regEx11 = new RegExp("\\bIF|\\bCASE|\\bWHILE|\\bVAR", "g");
+        let regEx12 = new RegExp("\\bELSIF|\\bELSE", "g");
+        let regEx13 = new RegExp(`\\bEND_IF;|\\bEND_CASE;|\\bEND_WHILE;|\\bEND_VAR`, "g");
+
+        const dynamicPattern = "\\(\\*|\\/\\/";
+        let regEx14 = new RegExp(dynamicPattern, "g");
+        for (var i = 0; i < lines.length; i++) {
+            lines[i] = lines[i].trim();
+            lines[i] = lines[i].trim();
+
+            if (!regEx14.test(lines[i])) {
+                if (regEx11.test(lines[i])) {
+                    lines[i] = "\t".repeat(tab) + lines[i];
+                    tab = tab + 1;
+                } else if (regEx12.test(lines[i])) {
+                    lines[i] = "\t".repeat(tab - 1) + lines[i];
+                } else if (regEx13.test(lines[i])) {
+                    if (tab > 0) {
+                        tab = tab - 1;
+                    }
+                    lines[i] = "\t".repeat(tab) + lines[i];
+                } else {
+                    lines[i] = "\t".repeat(tab) + lines[i];
+                }
+            } else {
+                lines[i] = "\t".repeat(tab) + lines[i];
+            }
+        }
+
+        text = lines.join("\n");
 
         return text;
     }
 
     capitalize(text: string): string {
-        let regEx = new RegExp(`(?<!\\\\(?:\\\\{2})*)${this.skipString.join('|')}|\\b(${this.types.join('|')}|${this.keywords.join('|')}|(?:END_)?(?:${this.ends.join('|')})|${this.functions.join('\\(|')}\\()\\b`, "ig");
+        let regEx = new RegExp(
+            `(?<!\\\\(?:\\\\{2})*)${this.skipString.join("|")}|\\b(${this.types.join(
+                "|"
+            )}|${this.keywords.join("|")}|(?:END_)?(?:${this.ends.join("|")})|${this.functions.join(
+                "\\(|"
+            )}\\()\\b`,
+            "ig"
+        );
         text = text.replace(regEx, (match, group) => {
             return group !== undefined ? match.toUpperCase() : match;
         });
 
-        text = text.replace(/(?<=T|DT|TOD|D)#[0-9\\:\\-\\_yYmMdDhHsS]+/ig, (match, group) => {
+        text = text.replace(/(?<=T|DT|TOD|D)#[0-9\\:\\-\\_yYmMdDhHsS]+/gi, (match, group) => {
             return group !== undefined ? match.toLowerCase() : match;
         });
 
